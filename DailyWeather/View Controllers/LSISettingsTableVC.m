fe0a3a87ff2b8f85d8ca3df456dafe13674c913e
Alexander Supe <alex.m.supe@gmail.com>
2020-03-19 12:18:15 +0100

    Basic View Setup Finished Settings

--- DailyWeather/View Controllers/LSISettingsTableVC.m
+++ DailyWeather/View Controllers/LSISettingsTableVC.m
@@ -18,16 +18,22 @@
     
 }
 
+- (IBAction)donePressed:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:(YES) completion:nil];
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return 2;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SettingsCell" forIndexPath:indexPath];
     
     if (indexPath.row == 0) {
+        cell.textLabel.text = @"Today's Weather v.1.0";
+    } else if (indexPath.row == 1) {
         cell.textLabel.text = @"Powered by Dark Sky";
         cell.textLabel.textColor = UIColor.systemBlueColor;
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
@@ -37,8 +43,8 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (indexPath.row == 0) {
+    [[tableView cellForRowAtIndexPath:indexPath] setSelected:NO];
+    if (indexPath.row == 1) {
         NSURL *url =  [NSURL URLWithString:@"https://darksky.net/poweredby/"];
         [UIApplication.sharedApplication openURL:url options:@{} completionHandler:nil];
     }
