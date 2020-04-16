de682ecc5415e6eae503d53a44b31fef50550433
Chris <chris.gonzales.82@gmail.com>
2020-04-16 12:40:58 -0700

    HourlyForecast .h and .m file implemented.

--- DailyWeather/LSIHourlyForecast.h
+++ DailyWeather/LSIHourlyForecast.h
@@ -12,6 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIHourlyForecast : NSObject
 
+@property NSDate *date;
+@property NSString *summary;
+@property NSString *icon;
+@property double temperature;
+@property double precipIntensity;
+@property double precipProbability;
+@property NSString *precipType;
+@property double humidity;
+@property double windSpeed;
+@property double windBearing;
+@property double uvIndex;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
