2c1a7a5d0ff0c2a339c1beba24d36b0b0c8d86bc
Chris <chris.gonzales.82@gmail.com>
2020-04-16 12:46:37 -0700

    Updated WeatherForecast .m and .h to take an array of the other forecast models, then updated .m  file.

--- DailyWeather/LSIWeatherForecast.h
+++ DailyWeather/LSIWeatherForecast.h
@@ -8,41 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+@class LSICurrentForecast;
+@class LSIDailyForecast;
+@class LSIHourlyForecast;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIWeatherForecast : NSObject
 
-//@property LSIWeatherForecast *CurrentForecast
-
-@property (nonatomic, readonly) NSDate *time;
-@property (nonatomic, readonly, copy) NSString *summary;
-@property (nonatomic, readonly, copy) NSString *icon;
-@property (nonatomic, readonly) NSNumber *precipProbability;
-@property (nonatomic, readonly) NSNumber *precipIntensity;
-@property (nonatomic, readonly) NSNumber *temperature;
-@property (nonatomic, readonly) NSNumber *apparentTemperature;
-@property (nonatomic, readonly) NSNumber *humidity;
-@property (nonatomic, readonly) NSNumber *pressure;
-@property (nonatomic, readonly) NSNumber *windSpeed;
-@property (nonatomic, readonly) NSNumber *windBearing;
-@property (nonatomic, readonly) NSNumber *uvIndex;
-
-
-- (instancetype)initWithTime:(NSDate *)time
-                     summary:(NSString *)summary
-                        icon:(NSString *)icon
-           precipProbability:(NSNumber *)precipProbability
-             precipIntensity:(NSNumber *)precipIntensity
-                 temperature:(NSNumber *)temperature
-         apparentTemperature:(NSNumber *)apparentTemperature
-                    humidity:(NSNumber *)humidity
-                    pressure:(NSNumber *)pressure
-                   windSpeed:(NSNumber *)windSpeed
-                 windBearing:(NSNumber *)windBearing
-                     uvIndex:(NSNumber *)uvIndex;
+@property LSICurrentForecast *current;
+@property NSArray<LSIDailyForecast *> *daily;
+@property NSArray<LSIHourlyForecast *> *hourly;
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
-
 
 @end
 
--- DailyWeather/LSIWeatherForecast.m
+++ DailyWeather/LSIWeatherForecast.m
@@ -7,115 +7,83 @@
 //
 
 #import "LSIWeatherForecast.h"
+#import "LSICurrentForecast.h"
+#import "LSIDailyForecast.h"
+#import "LSIHourlyForecast.h"
 
 @implementation LSIWeatherForecast
 
-//- (instancetype)initWithTime:(NSDate *)time
-//                     summary:(NSString *)summary
-//               icon:(NSString *)icon
-//  precipProbability:(NSNumber *)precipProbability
-//    precipIntensity:(NSNumber *)precipIntensity
-//        temperature:(NSNumber *)temperature
-//apparentTemperature:(NSNumber *)apparentTemperature
-//           humidity:(NSNumber *)humidity
-//           pressure:(NSNumber *)pressure
-//          windSpeed:(NSNumber *)windSpeed
-//        windBearing:(NSNumber *)windBearing
-//                     uvIndex:(NSNumber *)uvIndex {
-//    self = [super init];
-//    if (self) {
-//        _time = time;
-//        _summary = summary;
-//        _icon = icon;
-//        _precipProbability = precipProbability;
-//        _precipIntensity = precipIntensity;
-//        _temperature = temperature;
-//        _apparentTemperature = apparentTemperature;
-//        _humidity = humidity;
-//        _pressure = pressure;
-//        _windSpeed = windSpeed;
-//        _windBearing = windBearing;
-//        _uvIndex = uvIndex;
-//    }
-//    return self;
-//}
-
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
-    
-    // Containers
-    NSDictionary *currently = dictionary[@"currently"];
-    
-    // Non-optional values in JSON
-    NSNumber *timeInUnix = currently[@"time"];
-    
-    // Optional values in JSON
-    NSString *summary = currently[@"summary"];
-    NSString *icon = currently[@"icon"];
-    NSNumber *precipProbability = currently[@"precipProbability"];
-    NSNumber *precipIntensity = currently[@"precipIntensity"];
-    NSNumber *temperature = currently[@"temperature"];
-    NSNumber *apparentTemperature = currently[@"apparentTemperature"];
-    NSNumber *humidity = currently[@"humidity"];
-    NSNumber *pressure = currently[@"pressure"];
-    NSNumber *windSpeed = currently[@"windSpeed"];
-    NSNumber *windBearing = currently[@"windBearing"];
-    NSNumber *uvIndex= currently[@"uvIndex"];
-    
-    if ([summary isKindOfClass:[NSNull class]]) {
-        summary = nil;
-    }
-    if ([icon isKindOfClass:[NSNull class]]) {
-        icon = nil;
-    }
-    if ([precipProbability isKindOfClass:[NSNull class]]) {
-        precipProbability = nil;
-    }
-    if ([precipIntensity isKindOfClass:[NSNull class]]) {
-           precipIntensity = nil;
-       }
-    if ([temperature isKindOfClass:[NSNull class]]) {
-        temperature = nil;
-    }
-    if ([apparentTemperature isKindOfClass:[NSNull class]]) {
-        apparentTemperature = nil;
-    }
-    if ([humidity isKindOfClass:[NSNull class]]) {
-        humidity = nil;
-    }
-    if ([pressure isKindOfClass:[NSNull class]]) {
-        pressure = nil;
-    }
-    if ([windSpeed isKindOfClass:[NSNull class]]) {
-        windSpeed = nil;
-    }
-    if ([windBearing isKindOfClass:[NSNull class]]) {
-        windBearing = nil;
-    }
-    if ([uvIndex isKindOfClass:[NSNull class]]) {
-        uvIndex = nil;
-    }
-    
-    if (timeInUnix) {
-        _time = [NSDate dateWithTimeIntervalSince1970:timeInUnix.longValue];
-        _summary = summary;
-        _icon = icon;
-        _precipProbability = precipProbability;
-        _precipIntensity = precipIntensity;
-        _temperature = temperature;
-        _apparentTemperature = apparentTemperature;
-        _humidity = humidity;
-        _pressure = pressure;
-        _windSpeed = windSpeed;
-        _windBearing = windBearing;
-        _uvIndex = uvIndex;
-
-        //Move to View Controller
-//        NSDateFormatter *formatter= [[NSDateFormatter alloc] init];
-//        [formatter setLocale:[NSLocale currentLocale]];
-//        [formatter setDateFormat:@"dd.MM.yyyy"];
-//        NSString *dateString = [formatter stringFromDate:_time];
-    }
+    self = [super init];
+    if (self) {
+        NSNumber *latitudeNumber = dictionary[@"latitude"];
+        NSNumber *longitudeNumber = dictionary[@"longitude"];
+        NSDictionary *currently = dictionary[@"currently"];
+        NSDictionary *dailyOverview = dictionary[@"daily"];
+        NSArray<LSIDailyForecast *> *daily = dailyOverview[@"data"];
         
+        NSDictionary *hourlyOverview = dictionary[@"hourly"];
+        NSArray<LSIHourlyForecast *> *hourly = hourlyOverview[@"data"];
+        
+        LSICurrentForecast *currentForecast = [[LSICurrentForecast alloc] initWithDictionary:currently];
+        
+        NSMutableArray *dailyForecastArray = [[NSMutableArray alloc] init];
+        for (NSDictionary *day in daily) {
+            LSIDailyForecast *dailyForecast = [[LSIDailyForecast alloc] initWithDictionary:day];
+            [dailyForecastArray addObject: dailyForecast];
+        }
+        
+        NSMutableArray *hourlyForecastArray = [[NSMutableArray alloc] init];
+        for (NSDictionary *hour in hourly) {
+            LSIHourlyForecast *hourlyForecast = [[LSIHourlyForecast alloc] initWithDictionary:hour];
+            [hourlyForecastArray addObject: hourlyForecast];
+        }
+        
+        if (!(latitudeNumber && longitudeNumber)) {
+            NSLog(@"ERROR: Unable to parse JSON for WeatherForecast: %@", dictionary);
+            return nil;
+        }
+    
+        _current = currentForecast;
+        _daily = dailyForecastArray;
+        _hourly = hourlyForecastArray;
+    }
     return self;
 }
 @end
+
+// Should I verify data for all optional types in the .m for the corresponding model??
+
+//    if ([summary isKindOfClass:[NSNull class]]) {
+//        summary = nil;
+//    }
+//    if ([icon isKindOfClass:[NSNull class]]) {
+//        icon = nil;
+//    }
+//    if ([precipProbability isKindOfClass:[NSNull class]]) {
+//        precipProbability = nil;
+//    }
+//    if ([precipIntensity isKindOfClass:[NSNull class]]) {
+//           precipIntensity = nil;
+//       }
+//    if ([temperature isKindOfClass:[NSNull class]]) {
+//        temperature = nil;
+//    }
+//    if ([apparentTemperature isKindOfClass:[NSNull class]]) {
+//        apparentTemperature = nil;
+//    }
+//    if ([humidity isKindOfClass:[NSNull class]]) {
+//        humidity = nil;
+//    }
+//    if ([pressure isKindOfClass:[NSNull class]]) {
+//        pressure = nil;
+//    }
+//    if ([windSpeed isKindOfClass:[NSNull class]]) {
+//        windSpeed = nil;
+//    }
+//    if ([windBearing isKindOfClass:[NSNull class]]) {
+//        windBearing = nil;
+//    }
+//    if ([uvIndex isKindOfClass:[NSNull class]]) {
+//        uvIndex = nil;
+//    }
