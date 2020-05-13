16d5408b1436623f2c47b10271af2800defe6545
Hector <hectoremil21@hotmail.com>
2020-05-12 20:35:50 -0400

    LSIWeatherForecast.m Edit. Implement part of initWithDictionary.
    Check for all optional values and assign nil if NSNull class.

--- DailyWeather/LSIWeatherForecast.m
+++ DailyWeather/LSIWeatherForecast.m
@@ -10,4 +10,51 @@
 
 @implementation LSIWeatherForecast
 
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+
+        // No need for containers
+        // Temporary properties to check for nil
+        NSDate *time = dictionary[@"time"];
+
+        // Optional checker boilerplate code snippet;
+        // if([<#variable#> isKindOfClass:[NSNull class]]) { <#variable#> = nil; }
+        NSString *summary = dictionary[@"summary"];
+        if([summary isKindOfClass:[NSNull class]]) { summary = nil; }
+
+        NSString *icon = dictionary[@"icon"];
+        if([icon isKindOfClass:[NSNull class]]) { icon = nil; }
+
+        NSNumber *precipProbability = dictionary[@"precipProbability"];
+        if([precipProbability isKindOfClass:[NSNull class]]) { precipProbability = nil; }
+
+        NSNumber *precipIntensity = dictionary[@"precipIntensity"];
+        if([precipIntensity isKindOfClass:[NSNull class]]) { precipIntensity = nil; }
+
+        NSNumber *temperature = dictionary[@"temperature"];
+        if([temperature isKindOfClass:[NSNull class]]) { temperature = nil; }
+
+        NSNumber *apparentTemperature = dictionary[@"apparentTemperature"];
+        if([apparentTemperature isKindOfClass:[NSNull class]]) { apparentTemperature = nil; }
+
+        NSNumber *humidity = dictionary[@"humidity"];
+        if([humidity isKindOfClass:[NSNull class]]) { humidity = nil; }
+
+        NSNumber *pressure = dictionary[@"pressure"];
+        if([pressure isKindOfClass:[NSNull class]]) { pressure = nil; }
+
+        NSNumber *windSpeed = dictionary[@"windSpeed"];
+        if([windSpeed isKindOfClass:[NSNull class]]) { windSpeed = nil; }
+
+        NSNumber *windBearing = dictionary[@"windBearing"];
+        if([windBearing isKindOfClass:[NSNull class]]) { windBearing = nil; }
+
+        NSNumber *uvIndex = dictionary[@"uvIndex"];
+        if([uvIndex isKindOfClass:[NSNull class]]) { uvIndex = nil; }
+
+    }
+    return self;
+}
+
 @end
