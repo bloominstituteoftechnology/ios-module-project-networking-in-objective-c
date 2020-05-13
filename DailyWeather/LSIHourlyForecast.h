858e61a63b04d7aa0c6e09c1218c30cf5598c841
Hector <hectoremil21@hotmail.com>
2020-05-12 23:14:12 -0400

    Create LSIHourlyForecast object files.

---
+++ DailyWeather/LSIHourlyForecast.h
@@ -0,0 +1,17 @@
+//
+//  LSIHourlyForecast.h
+//  DailyWeather
+//
+//  Created by Karen Rodriguez on 5/12/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIHourlyForecast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/LSIHourlyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIHourlyForecast.m
+//  DailyWeather
+//
+//  Created by Karen Rodriguez on 5/12/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIHourlyForecast.h"
+
+@implementation LSIHourlyForecast
+
+@end
