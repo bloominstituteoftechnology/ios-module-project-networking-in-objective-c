426714f065c93587d1aac03d459025e0e97c8e05
Hector <hectoremil21@hotmail.com>
2020-05-12 22:15:24 -0400

    Implement initWith properties for DailyForecast.m

--- DailyWeather/LSIDailyForecast.m
+++ DailyWeather/LSIDailyForecast.m
@@ -10,4 +10,44 @@
 
 @implementation LSIDailyForecast
 
+- (instancetype)initWithTime:(NSDate *)time
+            summary:(NSString *)summary
+               icon:(NSString *)icon
+        sunriseTime:(NSNumber *)sunriseTime
+         sunsetTime:(NSNumber *)sunsetTime
+  precipProbability:(NSNumber *)precipProbability
+    precipIntensity:(NSNumber *)precipIntensity
+         precipType:(NSString *)precipType
+     temperatureLow:(NSNumber *)temperatureLow
+    temperatureHigh:(NSNumber *)temperatureHigh
+apparentTemperature:(NSNumber *)apparentTemperature
+           humidity:(NSNumber *)humidity
+           pressure:(NSNumber *)pressure
+          windSpeed:(NSNumber *)windSpeed
+        windBearing:(NSNumber *)windBearing
+                     uvIndex:(NSNumber *)uvIndex {
+    self = [super init];
+    if (self) {
+        _time = time;
+        _summary = [summary copy];
+        _icon = [icon copy];
+        _sunriseTime = sunriseTime;
+        _sunsetTime = sunsetTime;
+        _precipProbability = precipProbability;
+        _precipIntensity = precipIntensity;
+        _precipType = [precipType copy];
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windSpeed = windSpeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    return self;
+}
+
 @end
