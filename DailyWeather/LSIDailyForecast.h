3052aeaafc07b61f9f4db3eb2e3006f77007d62a
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-13 18:39:29 -0400

    Properties created fro Hourly and Daily

--- DailyWeather/LSIDailyForecast.h
+++ DailyWeather/LSIDailyForecast.h
@@ -12,6 +12,30 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIDailyForecast : NSObject
 
+@property (nonatomic, readonly) NSDate *time;
+@property (nonatomic, readonly, nullable, copy) NSString *summary;
+@property (nonatomic, readonly, nullable, copy) NSString *icon;
+@property (nonatomic, readonly, nullable) NSDate *sunriseTime;
+@property (nonatomic, readonly, nullable) NSDate *sunsetTime;
+@property (nonatomic, readonly, nullable) NSNumber *precipProbability;
+@property (nonatomic, readonly, nullable) NSNumber *precipIntensity;
+@property (nonatomic, readonly, nullable, copy) NSString *precipType;
+@property (nonatomic, readonly, nullable) NSNumber *temperatureLow;
+@property (nonatomic, readonly, nullable) NSNumber *temperatureHigh;
+@property (nonatomic, readonly, nullable) NSNumber *apparentTemperature;
+@property (nonatomic, readonly, nullable) NSNumber *humidity;
+@property (nonatomic, readonly, nullable) NSNumber *pressure;
+@property (nonatomic, readonly, nullable) NSNumber *windSpeed;
+@property (nonatomic, readonly, nullable) NSNumber *windBearing;
+@property (nonatomic, readonly, nullable) NSNumber *uvIndex;
+
+
+
+
+
+
+
+
 @end
 
 NS_ASSUME_NONNULL_END
