7c422de13a785fd41f75fa368db3788a1add728e
Hector <hectoremil21@hotmail.com>
2020-05-12 20:50:13 -0400

    initWith all properties implementation

--- DailyWeather/LSIWeatherForecast.m
+++ DailyWeather/LSIWeatherForecast.m
@@ -10,6 +10,36 @@
 
 @implementation LSIWeatherForecast
 
+- (instancetype)initWithTime:(NSDate *)time
+            summary:(NSString *)summary
+               icon:(NSString *)icon
+  precipProbability:(NSNumber *)precipProbability
+    precipIntensity:(NSNumber *)precipIntensity
+        temperature:(NSNumber *)temperature
+apparentTemperature:(NSNumber *)apparentTemperature
+           humidity:(NSNumber *)humidity
+           pressure:(NSNumber *)pressure
+          windSpeed:(NSNumber *)windSpeed
+        windBearing:(NSNumber *)windBearing
+                     uvIndex:(NSNumber *)uvIndex {
+    self = [super init];
+    if (self) {
+        _time = time;
+        _summary = [summary copy];
+        _icon = [icon copy];
+        _precipProbability = precipProbability;
+        _precipIntensity = precipIntensity;
+        _temperature = temperature;
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windSpeed = windSpeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+    return self;
+}
+
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
     self = [super init];
     if (self) {
