0ccb3f03529a13f86a7af7cba9b3b48e01da2ee0
Hector <hectoremil21@hotmail.com>
2020-05-12 21:06:20 -0400

    Finish initWithDictionary implementation. WeatherForecast

--- DailyWeather/LSIWeatherForecast.m
+++ DailyWeather/LSIWeatherForecast.m
@@ -11,16 +11,16 @@
 @implementation LSIWeatherForecast
 
 - (instancetype)initWithTime:(NSDate *)time
-            summary:(NSString *)summary
-               icon:(NSString *)icon
-  precipProbability:(NSNumber *)precipProbability
-    precipIntensity:(NSNumber *)precipIntensity
-        temperature:(NSNumber *)temperature
-apparentTemperature:(NSNumber *)apparentTemperature
-           humidity:(NSNumber *)humidity
-           pressure:(NSNumber *)pressure
-          windSpeed:(NSNumber *)windSpeed
-        windBearing:(NSNumber *)windBearing
+                     summary:(NSString *)summary
+                        icon:(NSString *)icon
+           precipProbability:(NSNumber *)precipProbability
+             precipIntensity:(NSNumber *)precipIntensity
+                 temperature:(NSNumber *)temperature
+         apparentTemperature:(NSNumber *)apparentTemperature
+                    humidity:(NSNumber *)humidity
+                    pressure:(NSNumber *)pressure
+                   windSpeed:(NSNumber *)windSpeed
+                 windBearing:(NSNumber *)windBearing
                      uvIndex:(NSNumber *)uvIndex {
     self = [super init];
     if (self) {
@@ -41,49 +41,47 @@ apparentTemperature:(NSNumber *)apparentTemperature
 }
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
-    self = [super init];
-    if (self) {
+    // No need for containers
+    // Temporary properties to check for nil
+    NSDate *time = dictionary[@"time"];
 
-        // No need for containers
-        // Temporary properties to check for nil
-        NSDate *time = dictionary[@"time"];
+    // Optional checker boilerplate code snippet;
+    // if([<#variable#> isKindOfClass:[NSNull class]]) { <#variable#> = nil; }
+    NSString *summary = dictionary[@"summary"];
+    if([summary isKindOfClass:[NSNull class]]) { summary = nil; }
 
-        // Optional checker boilerplate code snippet;
-        // if([<#variable#> isKindOfClass:[NSNull class]]) { <#variable#> = nil; }
-        NSString *summary = dictionary[@"summary"];
-        if([summary isKindOfClass:[NSNull class]]) { summary = nil; }
+    NSString *icon = dictionary[@"icon"];
+    if([icon isKindOfClass:[NSNull class]]) { icon = nil; }
 
-        NSString *icon = dictionary[@"icon"];
-        if([icon isKindOfClass:[NSNull class]]) { icon = nil; }
+    NSNumber *precipProbability = dictionary[@"precipProbability"];
+    if([precipProbability isKindOfClass:[NSNull class]]) { precipProbability = nil; }
 
-        NSNumber *precipProbability = dictionary[@"precipProbability"];
-        if([precipProbability isKindOfClass:[NSNull class]]) { precipProbability = nil; }
+    NSNumber *precipIntensity = dictionary[@"precipIntensity"];
+    if([precipIntensity isKindOfClass:[NSNull class]]) { precipIntensity = nil; }
 
-        NSNumber *precipIntensity = dictionary[@"precipIntensity"];
-        if([precipIntensity isKindOfClass:[NSNull class]]) { precipIntensity = nil; }
+    NSNumber *temperature = dictionary[@"temperature"];
+    if([temperature isKindOfClass:[NSNull class]]) { temperature = nil; }
 
-        NSNumber *temperature = dictionary[@"temperature"];
-        if([temperature isKindOfClass:[NSNull class]]) { temperature = nil; }
+    NSNumber *apparentTemperature = dictionary[@"apparentTemperature"];
+    if([apparentTemperature isKindOfClass:[NSNull class]]) { apparentTemperature = nil; }
 
-        NSNumber *apparentTemperature = dictionary[@"apparentTemperature"];
-        if([apparentTemperature isKindOfClass:[NSNull class]]) { apparentTemperature = nil; }
+    NSNumber *humidity = dictionary[@"humidity"];
+    if([humidity isKindOfClass:[NSNull class]]) { humidity = nil; }
 
-        NSNumber *humidity = dictionary[@"humidity"];
-        if([humidity isKindOfClass:[NSNull class]]) { humidity = nil; }
+    NSNumber *pressure = dictionary[@"pressure"];
+    if([pressure isKindOfClass:[NSNull class]]) { pressure = nil; }
 
-        NSNumber *pressure = dictionary[@"pressure"];
-        if([pressure isKindOfClass:[NSNull class]]) { pressure = nil; }
+    NSNumber *windSpeed = dictionary[@"windSpeed"];
+    if([windSpeed isKindOfClass:[NSNull class]]) { windSpeed = nil; }
 
-        NSNumber *windSpeed = dictionary[@"windSpeed"];
-        if([windSpeed isKindOfClass:[NSNull class]]) { windSpeed = nil; }
+    NSNumber *windBearing = dictionary[@"windBearing"];
+    if([windBearing isKindOfClass:[NSNull class]]) { windBearing = nil; }
 
-        NSNumber *windBearing = dictionary[@"windBearing"];
-        if([windBearing isKindOfClass:[NSNull class]]) { windBearing = nil; }
+    NSNumber *uvIndex = dictionary[@"uvIndex"];
+    if([uvIndex isKindOfClass:[NSNull class]]) { uvIndex = nil; }
 
-        NSNumber *uvIndex = dictionary[@"uvIndex"];
-        if([uvIndex isKindOfClass:[NSNull class]]) { uvIndex = nil; }
+    self = [self initWithTime:time summary:summary icon:icon precipProbability:precipProbability precipIntensity:precipIntensity temperature:temperature apparentTemperature:apparentTemperature humidity:humidity pressure:pressure windSpeed:windSpeed windBearing:windBearing uvIndex:uvIndex];
 
-    }
     return self;
 }
 
