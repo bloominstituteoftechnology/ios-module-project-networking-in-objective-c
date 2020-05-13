02ed31c2c60648a10dd80219adb8101183f50b71
Hector <hectoremil21@hotmail.com>
2020-05-12 22:02:17 -0400

    Test for DailyForecast. Create LSIDailyForecast object files

---
+++ DailyWeather/LSIDailyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIDailyForecast.m
+//  DailyWeather
+//
+//  Created by Karen Rodriguez on 5/12/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyForecast.h"
+
+@implementation LSIDailyForecast
+
+@end
