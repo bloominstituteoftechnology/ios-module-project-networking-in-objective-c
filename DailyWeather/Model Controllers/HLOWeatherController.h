359869e0ce1fe0642886b341d93689874c3ed2b4
Hector <hectoremil21@hotmail.com>
2020-05-13 21:34:02 -0400

    Forgot to add initials to name.

--- DailyWeather/Model Controllers/HLOWeatherController.h
+++ DailyWeather/Model Controllers/HLOWeatherController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WeatherController : NSObject
+@interface HLOWeatherController : NSObject
 
 @end
 
---
+++ DailyWeather/Model Controllers/HLOWeatherController.m
@@ -0,0 +1,25 @@
+//
+//  WeatherController.m
+//  DailyWeather
+//
+//  Created by Karen Rodriguez on 5/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "HLOWeatherController.h"
+
+// Class forward all models
+@class LSIWeatherForecast;
+@class LSIDailyForecast;
+@class LSIHourlyForecast;
+
+@implementation HLOWeatherController
+
+// What I'll need...
+// A current weather property
+// An array of hourly weather
+// An array of daily weather
+// Method for fetching from API
+
+// Method for parsing the data separately (So that I may call this method with mock data)
+@end
