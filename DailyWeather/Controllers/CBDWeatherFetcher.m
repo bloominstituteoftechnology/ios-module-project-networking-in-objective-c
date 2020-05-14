4bd75e92b2e28ad140150b14f868b98100c091e4
Christopher DeVito <christopher.devito@protonmail.com>
2020-05-14 16:42:06 -0400

    Stubbed method to fetch forecast. designed daily ui.

---
+++ DailyWeather/Controllers/CBDWeatherFetcher.m
@@ -0,0 +1,18 @@
+//
+//  CBDWeatherFetcher.m
+//  DailyWeather
+//
+//  Created by Christopher Devito on 5/14/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "CBDWeatherFetcher.h"
+
+@implementation CBDWeatherFetcher
+
+- (void)fetchWeatherAtLatitude:(NSNumber *)latitude
+      longitude:(NSNumber *)longitude
+               completionBlock:(CBDWeatherFetcherCompletion)completionBlock {
+    
+}
+@end
