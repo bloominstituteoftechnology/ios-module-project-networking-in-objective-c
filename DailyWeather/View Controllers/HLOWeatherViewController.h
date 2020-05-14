2db9981e1e278b3c6b229cb89c6d657d68ce7ee9
Hector <hectoremil21@hotmail.com>
2020-05-13 20:23:10 -0400

    Create HLOViewController for Day 2 of the project.

---
+++ DailyWeather/View Controllers/HLOWeatherViewController.h
@@ -0,0 +1,17 @@
+//
+//  HLOWeatherViewController.h
+//  DailyWeather
+//
+//  Created by Karen Rodriguez on 5/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLOWeatherViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
