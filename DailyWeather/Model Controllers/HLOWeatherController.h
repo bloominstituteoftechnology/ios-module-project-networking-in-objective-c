9e9229b52a5b53ac260953709f862cb866613c88
Hector <hectoremil21@hotmail.com>
2020-05-13 21:35:23 -0400

    Was accidentally writing everything into .m file instead of .h

--- DailyWeather/Model Controllers/HLOWeatherController.h
+++ DailyWeather/Model Controllers/HLOWeatherController.h
@@ -8,10 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+// Class forward all models
+@class LSIWeatherForecast;
+@class LSIDailyForecast;
+@class LSIHourlyForecast;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HLOWeatherController : NSObject
 
+// What I'll need...
+// A current weather property
+@property (nonatomic, readonly) LSIWeatherForecast *currentWeather;
+// An array of hourly weather
+// An array of daily weather
+// Method for fetching from API
+
+// Method for parsing the data separately (So that I may call this method with mock data)
 @end
 
 NS_ASSUME_NONNULL_END
