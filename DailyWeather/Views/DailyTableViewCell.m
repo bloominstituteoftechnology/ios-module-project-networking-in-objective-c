2a2d85dcc29e12bef5df5b174d668fb0308b2bb2
Hector <hectoremil21@hotmail.com>
2020-05-13 23:31:57 -0400

    Implement didSet by overriding setter for forecast property in DailyCell.m. Implement updateViews.

--- DailyWeather/Views/DailyTableViewCell.m
+++ DailyWeather/Views/DailyTableViewCell.m
@@ -7,6 +7,9 @@
 //
 
 #import "DailyTableViewCell.h"
+#import "LSIDailyForecast.h"
+#import "LSIWeatherIcons.h"
+
 
 // Aha... so the () is what seems to tell the compiler, hey this is extra/private... interesting.
 @interface DailyTableViewCell ()
@@ -16,6 +19,8 @@
 @property (strong, nonatomic) IBOutlet UILabel *tempHighLabel;
 @property (strong, nonatomic) IBOutlet UILabel *tempLowLabel;
 
+// MARK:-  Properties
+@property (nonatomic) LSIDailyForecast *forecast;
 @end
 
 @implementation DailyTableViewCell
@@ -31,4 +36,23 @@
     // Configure the view for the selected state
 }
 
+// MARK:- Override setter
+- (void)setForecast:(LSIDailyForecast *)forecast {
+    // Setter
+    _forecast = forecast;
+
+    // didSet
+    [self updateViews];
+}
+
+- (void)updateViews {
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:(int)self.forecast.time];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEEE"];
+
+    self.dayLabel.text = [dateFormatter stringFromDate:date];
+    self.iconLabel.image = [LSIWeatherIcons weatherImageForIconName:self.forecast.icon];
+    self.tempHighLabel.text = self.forecast.temperatureHigh.stringValue;
+    self.tempLowLabel.text = self.forecast.temperatureLow.stringValue;
+}
 @end
