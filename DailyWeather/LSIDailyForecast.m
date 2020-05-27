1bc8e7a3eb924efe054873c87fa7da130597ed36
Sal <salmanamer@gmail.com>
2020-05-27 01:48:16 -0700

    setup LSIHourlyForecast and LSIDailyForecast class files

---
+++ DailyWeather/LSIDailyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIDailyForecast.m
+//  DailyWeather
+//
+//  Created by Sal B Amer on 5/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyForecast.h"
+
+@implementation LSIDailyForecast
+
+@end
---
+++ DailyWeather/LSIHourlyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIHourlyForecast.m
+//  DailyWeather
+//
+//  Created by Sal B Amer on 5/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIHourlyForecast.h"
+
+@implementation LSIHourlyForecast
+
+@end
