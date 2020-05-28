b72ef83167dc41693eed0a7bb26d472a2f03c931
David Wright <davidtwright89@gmail.com>
2020-05-28 02:05:02 -0400

    Added an LSIWeatherForcast model class

---
+++ DailyWeather/Model/LSIWeatherForcast.h
@@ -0,0 +1,17 @@
+//
+//  LSIWeatherForcast.h
+//  DailyWeather
+//
+//  Created by David Wright on 5/28/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIWeatherForcast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Model/LSIWeatherForcast.m
@@ -0,0 +1,13 @@
+//
+//  LSIWeatherForcast.m
+//  DailyWeather
+//
+//  Created by David Wright on 5/28/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIWeatherForcast.h"
+
+@implementation LSIWeatherForcast
+
+@end
