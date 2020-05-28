79d8dd0c536d8f656879ec09b207a049751af52b
David Wright <davidtwright89@gmail.com>
2020-05-28 03:35:13 -0400

    Created tests for parsing the JSON from the CurrentWeather.json file

--- DailyWeatherTests/DailyWeatherTests.m
+++ DailyWeatherTests/DailyWeatherTests.m
@@ -7,6 +7,10 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "../DailyWeather/LambdaSDK/LSIFileHelper.h"
+#import "../DailyWeather/LambdaSDK/LSILog.h"
+#import "../DailyWeather/Model/LSIWeatherForcast.h"
+#import "../DailyWeather/Model/LSICurrentForecast.h"
 
 @interface DailyWeatherTests : XCTestCase
 
@@ -14,12 +18,53 @@
 
 @implementation DailyWeatherTests
 
-- (void)testExample {
+- (void)testWeatherParsing {
 
     // TODO: Use LSIFileHelper to load JSON from your test bundle
+    NSData *weatherData = loadFile(@"CurrentWeather.json", [DailyWeatherTests class]);
+    
+    NSError *jsonError = nil;
+    NSDictionary *weatherDictionary = [NSJSONSerialization JSONObjectWithData:weatherData options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:&jsonError]; // NSError **
+    if (!weatherDictionary) {
+        NSLog(@"We've got an error: %@", jsonError);
+    }
+    
+    if (![weatherDictionary isKindOfClass:[NSDictionary class]]) {
+        NSLog(@"weatherDictionary is not a dictionary!");
+        return;
+    }
+    
+    LSICurrentForecast *currentForecast = [[LSICurrentForecast alloc] initWithDictionary:weatherDictionary];
+    
+    NSLog(@"weather: %@", currentForecast);
     
     // TODO: Create Unit Tests for each separate JSON file
+    
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:1581003354];
+    NSString *summary = @"Clear";
+    NSString *icon = @"clear-day";
+    double precipIntensity = 0;
+    double precipProbability = 0;
+    double temperature = 48.35;
+    double apparentTemperature = 47.4;
+    double humidity = 0.77;
+    double pressure = 1023.2;
+    double windSpeed = 3.45;
+    double windBearing = 24;
+    double uvIndex = 0;
 
+    XCTAssertEqualObjects(time, currentForecast.time);
+    XCTAssertEqualObjects(summary, currentForecast.summary);
+    XCTAssertEqualObjects(icon, currentForecast.icon);
+    XCTAssertEqualWithAccuracy(precipIntensity, currentForecast.precipIntensity, 0.0001);
+    XCTAssertEqualWithAccuracy(precipProbability, currentForecast.precipProbability, 0.0001);
+    XCTAssertEqualWithAccuracy(temperature, currentForecast.temperature, 0.0001);
+    XCTAssertEqualWithAccuracy(apparentTemperature, currentForecast.apparentTemperature, 0.0001);
+    XCTAssertEqualWithAccuracy(humidity, currentForecast.humidity, 0.0001);
+    XCTAssertEqualWithAccuracy(pressure, currentForecast.pressure, 0.0001);
+    XCTAssertEqualWithAccuracy(windSpeed, currentForecast.windSpeed, 0.0001);
+    XCTAssertEqualWithAccuracy(windBearing, currentForecast.windBearing, 0.0001);
+    XCTAssertEqualWithAccuracy(uvIndex, currentForecast.uvIndex, 0.0001);
 }
 
 @end
