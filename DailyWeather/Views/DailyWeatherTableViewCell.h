046aefa6249dfbd524925659f2420360f92ccf92
Sal <salmanamer@gmail.com>
2020-05-29 06:05:02 -0700

    setup DailyWeatherTableViewCell.h

--- DailyWeather/Views/DailyWeatherTableViewCell.h
+++ DailyWeather/Views/DailyWeatherTableViewCell.h
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+@class LSIDailyForecast;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DailyWeatherTableViewCell : UITableViewCell
+@property (nonatomic) LSIDailyForecast *forecast;
 
 @end
 
