d7dded581a5ef69de59da5184bc37206447265ba
Sal <salmanamer@gmail.com>
2020-05-29 03:48:03 -0700

    setup NetworkingUI Branch after fixing errors and created LSIWeatherFetcher

---
+++ LSIWeatherFetcher.h
@@ -0,0 +1,17 @@
+//
+//  LSIWeatherFetcher.h
+//  DailyWeather
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIWeatherFetcher : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LSIWeatherFetcher.m
@@ -0,0 +1,13 @@
+//
+//  LSIWeatherFetcher.m
+//  DailyWeather
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIWeatherFetcher.h"
+
+@implementation LSIWeatherFetcher
+
+@end
