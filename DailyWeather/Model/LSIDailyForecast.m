ce021aa2b24b6a8699bc4ef2de7bb61787df61c9
David Wright <davidtwright89@gmail.com>
2020-05-28 22:03:49 -0400

    Added an LSIDailyForecast class

---
+++ DailyWeather/Model/LSIDailyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIDailyForecast.m
+//  DailyWeather
+//
+//  Created by David Wright on 5/28/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyForecast.h"
+
+@implementation LSIDailyForecast
+
+@end
