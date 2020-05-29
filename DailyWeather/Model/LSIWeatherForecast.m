6eeb264d9d23ab43f45a49dce80390fc80ae0c1f
David Wright <davidtwright89@gmail.com>
2020-05-29 00:21:33 -0400

    Updated the implementation of the initializers in the LSIWeatherForecast class

--- DailyWeather/Model/LSIWeatherForecast.m
+++ DailyWeather/Model/LSIWeatherForecast.m
@@ -8,23 +8,105 @@
 
 #import "LSIWeatherForecast.h"
 #import "LSICurrentForecast.h"
+#import "LSIDailyForecast.h"
+#import "LSIHourlyForecast.h"
 
 @implementation LSIWeatherForecast
 
-- (instancetype)initWithCurrentForecast:(LSICurrentForecast *)currentForecast
+- (instancetype)initWithLatitude:(double)latitude
+                       longitude:(double)longitude
+                        timeZone:(NSString *)timeZone
+                 currentForecast:(LSIDailyForecast *)currently
+                   dailyForecast:(NSArray<LSIDailyForecast *> *)daily
+                  hourlyForecast:(NSArray<LSIDailyForecast *> *)hourly
 {
     if (self = [super init]) {
-        _currentForecast = currentForecast;
+        _latitude = latitude;
+        _longitude = longitude;
+        _timeZone = timeZone.copy;
+        _currently = currently.copy;
+        _daily = daily.copy;
+        _hourly = hourly.copy;
     }
     return self;
 }
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
-    LSICurrentForecast *currentForecast = [[LSICurrentForecast alloc] initWithDictionary:dictionary];
+    NSNumber *latitude = [dictionary objectForKey:@"latitude"];
+    if (![latitude isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSNumber *longitude = [dictionary objectForKey:@"longitude"];
+    if (![longitude isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSString *timeZone = [dictionary objectForKey:@"timeZone"];
+    if ([timeZone isKindOfClass:[NSNull class]]) {
+        timeZone = nil;
+    } else if (![timeZone isKindOfClass:[NSString class]]) return nil;
+    
+    // Current Forecast Data
+    
+    NSDictionary *currentForecastDictionary = [dictionary objectForKey:@"currently"];
+    if ([currentForecastDictionary isKindOfClass:[NSNull class]]) {
+        currentForecastDictionary = nil;
+    } else if (![currentForecastDictionary isKindOfClass:[NSDictionary class]]) return nil;
+    
+    LSICurrentForecast *currentForecast = [[LSICurrentForecast alloc] initWithDictionary:currentForecastDictionary];
     if (![currentForecast isKindOfClass:[NSDictionary class]]) return nil;
     
-    return [self initWithCurrentForecast:currentForecast];
+    // Daily Forecast Data
+    
+    NSDictionary *dailyDictionary = [dictionary objectForKey:@"daily"];
+    if (![dailyDictionary isKindOfClass:[NSDictionary class]]) return nil;
+    
+    NSArray *dailyDataDictionaries = [dailyDictionary objectForKey:@"data"];
+    if (![dailyDataDictionaries isKindOfClass:[NSArray class]]) return nil;
+    
+    NSMutableArray *dailyForecasts = [[NSMutableArray alloc] initWithCapacity:dailyDataDictionaries.count];
+    
+    for (NSDictionary *dailyDataDictionary in dailyDataDictionaries) {
+        if (![dailyDataDictionary isKindOfClass:[NSDictionary class]]) continue;
+        
+        LSIDailyForecast *dailyForecast = [[LSIDailyForecast alloc] initWithDictionary:dailyDataDictionary];
+        
+        if (dailyForecast) {
+            [dailyForecasts addObject:dailyForecast];
+        } else {
+            // One of our "required" fields might be optional and we may need to debug this with real data
+            NSLog(@"Unable to parse daily data dictionary: %@", dailyDataDictionary);
+        }
+    }
+    
+    // Hourly Forecast Data
+
+    NSDictionary *hourlyDictionary = [dictionary objectForKey:@"hourly"];
+    if (![hourlyDictionary isKindOfClass:[NSDictionary class]]) return nil;
+    
+    NSArray *hourlyDataDictionaries = [hourlyDictionary objectForKey:@"data"];
+    if (![hourlyDataDictionaries isKindOfClass:[NSArray class]]) return nil;
+    
+    NSMutableArray *hourlyForecasts = [[NSMutableArray alloc] initWithCapacity:hourlyDataDictionaries.count];
+    
+    for (NSDictionary *hourlyDataDictionary in hourlyDataDictionaries) {
+        if (![hourlyDataDictionary isKindOfClass:[NSDictionary class]]) continue;
+        
+        LSIHourlyForecast *hourlyForecast = [[LSIHourlyForecast alloc] initWithDictionary:hourlyDataDictionary];
+        
+        if (hourlyForecast) {
+            [hourlyForecasts addObject:hourlyForecast];
+        } else {
+            // One of our "required" fields might be optional and we may need to debug this with real data
+            NSLog(@"Unable to parse hourly data dictionary: %@", hourlyDataDictionary);
+        }
+    }
+    
+    
+    return [self initWithLatitude:latitude.doubleValue
+                        longitude:longitude.doubleValue
+                         timeZone:timeZone
+                  currentForecast:currentForecast
+                    dailyForecast:dailyForecasts
+                   hourlyForecast:hourlyForecasts];
 }
 
 @end
