fcc708d55554bb53e2fbf61bb46a80411eb6e6f7
David Wright <davidtwright89@gmail.com>
2020-05-30 15:28:47 -0400

    Added a WeatherFetcher class

---
+++ DailyWeather/Model/WeatherFetcher.m
@@ -0,0 +1,13 @@
+//
+//  WeatherFetcher.m
+//  DailyWeather
+//
+//  Created by David Wright on 5/30/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "WeatherFetcher.h"
+
+@implementation WeatherFetcher
+
+@end
