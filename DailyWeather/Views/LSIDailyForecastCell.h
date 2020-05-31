2c90c7b01df2eac4f487f724020bcdd05b9e5683
David Wright <davidtwright89@gmail.com>
2020-05-31 18:10:31 -0400

    Added an LSIDailyForecastCell class.

---
+++ DailyWeather/Views/LSIDailyForecastCell.h
@@ -0,0 +1,17 @@
+//
+//  LSIDailyForecastCell.h
+//  DailyWeather
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIDailyForecastCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Views/LSIDailyForecastCell.m
@@ -0,0 +1,24 @@
+//
+//  LSIDailyForecastCell.m
+//  DailyWeather
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyForecastCell.h"
+
+@implementation LSIDailyForecastCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
