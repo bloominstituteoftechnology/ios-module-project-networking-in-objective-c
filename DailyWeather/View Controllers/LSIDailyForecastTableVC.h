4c9b201c7fb62e860600d2717c21127cc198c130
David Wright <davidtwright89@gmail.com>
2020-05-31 18:08:34 -0400

    Created an LSIDailyForecastTableVC class

---
+++ DailyWeather/View Controllers/LSIDailyForecastTableVC.h
@@ -0,0 +1,17 @@
+//
+//  LSIDailyForecastTableVC.h
+//  DailyWeather
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIDailyForecastTableVC : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
