3d7b4b660116fc326acb1454d1a11d190c9b647e
Brad <braddiroff@icloud.com>
2020-06-10 18:27:38 -0400

    Added the models

---
+++ DailyWeather/LSIHourlyForecast.h
@@ -0,0 +1,17 @@
+//
+//  LSIHourlyForecast.h
+//  DailyWeather
+//
+//  Created by Bradley Diroff on 6/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIHourlyForecast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
