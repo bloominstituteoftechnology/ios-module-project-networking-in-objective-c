c5292832e7ee615df2ca4c2c726b255ce7bee8c8
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-10 21:10:35 -0700

    Added super intializer in .m for weather forcast

--- DailyWeather/LSIWeatherForcast.m
+++ DailyWeather/LSIWeatherForcast.m
@@ -10,5 +10,35 @@
 
 @implementation LSIWeatherForcast
 
+- (instancetype)initWithTime:(NSDate *)time
+            summary:(NSString *)summary
+               icon:(NSString *)icon
+  precipProbability:(NSNumber *)precipProbability
+   precipeIntensity:(NSNumber *)precipeIntensity
+        temperature:(NSNumber *)temperature
+apparentTemperature:(NSNumber *)apparentTemperature
+           humidity:(NSNumber *)humidity
+           pressure:(NSNumber *)pressure
+          windSpeed:(NSNumber *)windSpeed
+        windBearing:(NSNumber *)windBearing
+            uvIndex:(NSNumber *)uvIndex
+{
+    self = [super init];
+    if (self) {
+        _time = time;
+        _summary = summary;
+        _icon = icon;
+        _precipProbability = precipProbability;
+        _precipIntensity = precipeIntensity;
+        _temperature = temperature;
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windSpeed = windSpeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+    return self;
+}
 
 @end
