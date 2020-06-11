3b3392702a7d9697de5a95f01dede011c965dd14
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-10 20:54:02 -0700

    Added the daily cast forecast models

---
+++ DailyWeather/LSIDailyForcast.h
@@ -0,0 +1,17 @@
+//
+//  LSIDailyForcast.h
+//  DailyWeather
+//
+//  Created by Bhawnish Kumar on 6/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIDailyForcast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/LSIDailyForcast.m
@@ -0,0 +1,13 @@
+//
+//  LSIDailyForcast.m
+//  DailyWeather
+//
+//  Created by Bhawnish Kumar on 6/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyForcast.h"
+
+@implementation LSIDailyForcast
+
+@end
