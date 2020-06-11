1631f1eefc768cf52be5efd5958fa4563aea53e1
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-10 20:47:56 -0700

    Added initializer for the properties

--- DailyWeather/LSIWeatherForcast.h
+++ DailyWeather/LSIWeatherForcast.h
@@ -13,17 +13,20 @@ NS_ASSUME_NONNULL_BEGIN
 @interface LSIWeatherForcast : NSObject
 
 @property (nonatomic) NSDate *time;
-@property (nonatomic, nullable, readonly) NSString *summaryç;
+@property (nonatomic, nullable, readonly) NSString *summary;
 @property(nonatomic, nullable, readonly)  NSString *icon;
 @property (nonatomic, nullable, readonly) NSNumber *precipProbability;
 @property (nonatomic, nullable, readonly) NSNumber *precipIntensity;
-@property (nonatomic, nullable, readonly) NSNumber *temperatur;
-@property (nonatomic, nullable, readonly) NSNumber *apperantTemperature;
+@property (nonatomic, nullable, readonly) NSNumber *temperature;
+@property (nonatomic, nullable, readonly) NSNumber *apparentTemperature;
 @property (nonatomic, nullable, readonly) NSNumber *humidity;
 @property (nonatomic, nullable, readonly) NSNumber *pressure;
 @property (nonatomic, nullable, readonly) NSNumber *windSpeed;
 @property (nonatomic, nullable, readonly) NSNumber *windBearing;
 @property (nonatomic, nullable, readonly) NSNumber *uvIndex;
+
+- (instancetype)initWithTime:(NSDate *)time summary:(NSString *)summary icon:(NSString *)icon precipProbability:(NSNumber *)precipProbability precipeIntensity:(NSNumber *)precipeIntensity temperature:(NSNumber *)temperature apparentTemperature:(NSNumber *)apparentTemperature humidity:(NSNumber *)humidity pressure:(NSNumber *)pressure windSpeed:(NSNumber *)windSpeed windBearing:(NSNumber *)windBearing uvIndex:(NSNumber *)uvIndex;
+
 
 
 @end
