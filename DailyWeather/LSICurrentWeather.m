158daf852ef226ad7ca083055002e3633450e4b2
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-11 18:11:17 -0700

    Added the required properties for .m currentweather file

--- DailyWeather/LSICurrentWeather.m
+++ DailyWeather/LSICurrentWeather.m
@@ -10,4 +10,33 @@
 
 @implementation LSICurrentWeather
 
+- (instancetype)initWithTime:(NSDate *)time summary:(NSString *)summary icon:(NSString *)icon precipProbability:(NSNumber *)precipProbability precipIntensity:(NSNumber *)precipIntensity temperature:(double)temperature apparentTemperature:(double)apparentTemperature humidity:(NSNumber *)humidity pressure:(NSNumber *)pressure windSpeed:(NSNumber *)windSpeed windBearing:(NSNumber *)windBearing uvIndex:(NSNumber *)uvIndex {
+    
+    self = [super init];
+    if (self) {
+        
+        _time = time;
+        _summary = [summary copy];
+        _icon = [icon copy];
+        _precipProbability = precipProbability;
+        _precipIntensity = precipIntensity;
+        _temperature = temperature;
+        _apparentTemperature = apparentTemperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windSpeed = windSpeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+    return self;
+    
+}
+
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    
+    return nil;
+}
+
+
 @end
