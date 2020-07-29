b698f9d9926481a0df05e61fb177f4ea5672b17c
Claudia Maciel <clc_80@yahoo.com>
2020-07-28 20:31:19 -0700

    Added class checks

--- DailyWeather/Models/CurrentForecast.m
+++ DailyWeather/Models/CurrentForecast.m
@@ -59,17 +59,50 @@
     if (![dictionary isKindOfClass:[NSDictionary class]]) return nil;
 
     NSNumber *timeInMilliseconds = dictionary[@"time"];
+    if ([timeInMilliseconds isKindOfClass:NSNull.class]) timeInMilliseconds = nil;
+    else if (![timeInMilliseconds isKindOfClass:NSNumber.class]) return nil;
+    
     NSDate *time = [NSDate dateWithTimeIntervalSince1970:timeInMilliseconds.longValue/1000.0];
+    
     NSString *summary = dictionary[@"summary"];
+    if ([summary isKindOfClass:NSNull.class]) summary = nil;
+    else if (![summary isKindOfClass:NSString.class]) return nil;
+    
     NSString *icon = dictionary[@"icon"];
+    if ([icon isKindOfClass:[NSNull class]]) icon = nil;
+    else if (![icon isKindOfClass:[NSString class]]) return nil;
+    
     NSNumber *precipProbability = dictionary[@"precipProbability"];
+    if ([precipProbability isKindOfClass:[NSNull class]]) precipProbability = nil;
+    else if (![precipProbability isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *precipIntensity = dictionary[@"precipIntensity"];
+    if ([precipIntensity isKindOfClass:[NSNull class]]) precipIntensity = nil;
+    else if (![precipIntensity isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *temperature = dictionary[@"temperature"];
+    if ([temperature isKindOfClass:[NSNull class]]) temperature = nil;
+    else if (![temperature isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *humidity = dictionary[@"humidity"];
+    if ([humidity isKindOfClass:[NSNull class]]) humidity = nil;
+    else if (![humidity isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *pressure = dictionary[@"pressure"];
+    if ([pressure isKindOfClass:[NSNull class]]) pressure = nil;
+    else if (![pressure isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *windSpeed = dictionary[@"windSpeed"];
+    if ([windSpeed isKindOfClass:[NSNull class]]) windSpeed = nil;
+    else if (![windSpeed isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *windBearing = dictionary[@"windBearing"];
+    if ([windBearing isKindOfClass:[NSNull class]]) windBearing = nil;
+    else if (![windBearing isKindOfClass:[NSNumber class]]) return nil;
+    
     NSNumber *uvIndex = dictionary[@"uvIndex"];
+    if ([uvIndex isKindOfClass:[NSNull class]]) uvIndex = nil;
+    else if (![uvIndex isKindOfClass:[NSNumber class]]) return nil;
 
     return [self initWithTime:time
                       summary:summary.copy
