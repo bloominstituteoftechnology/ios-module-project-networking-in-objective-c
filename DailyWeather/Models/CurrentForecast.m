76ce7cff95cd486ad9d43c6d837e073cbec165e0
Claudia Maciel <clc_80@yahoo.com>
2020-07-28 19:37:36 -0700

    Moved properties and methods to correct file and implemented the methods

--- DailyWeather/Models/CurrentForecast.m
+++ DailyWeather/Models/CurrentForecast.m
@@ -7,7 +7,67 @@
 //
 
 #import "CurrentForecast.h"
+#import "LSIWeatherForecast.h"
 
 @implementation CurrentForecast
+
+- (instancetype)initWithTime:(NSDate *)time
+                     summary:(NSString *)summary
+                        icon:(NSString *)icon
+           precipProbability:(int)precipProbability
+             precipIntensity:(int)precipIntensity
+                 temperature:(double)temperature
+                    humidity:(double)humidity
+                    pressure:(double)pressure
+                   windSpeed:(double)windSpeed
+                 windBearing:(NSString *)windBearing
+                     uvIndex:(int)uvIndex
+{
+    if (self = [super init]) {
+        _time = time;
+        _summary = summary;
+        _icon = icon;
+        _precipProbability = precipProbability;
+        _precipIntensity = precipIntensity;
+        _temperature = temperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        _windSpeed = windSpeed;
+        _windBearing = windBearing;
+        _uvIndex = uvIndex;
+    }
+    return self;
+}
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    if (![dictionary isKindOfClass:[NSDictionary class]]) return nil;
+
+    NSNumber *timeInMilliseconds = dictionary[@"time"];
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:timeInMilliseconds.longValue/1000.0];
+    NSString *summary = dictionary[@"summary"];
+    NSString *icon = dictionary[@"icon"];
+    NSNumber *precipProbability = dictionary[@"precipProbability"];
+    NSNumber *precipIntensity = dictionary[@"precipIntensity"];
+    NSNumber *temperature = dictionary[@"temperature"];
+    NSNumber *humidity = dictionary[@"humidity"];
+    NSNumber *pressure = dictionary[@"pressure"];
+    NSNumber *windSpeed = dictionary[@"windSpeed"];
+    NSString *windBearing = dictionary[@"windBearing"];
+    NSNumber *uvIndex = dictionary[@"uvIndex"];
+
+    self = [self initWithTime:time
+                      summary:summary.copy
+                         icon:icon
+            precipProbability:precipProbability.intValue
+              precipIntensity:precipIntensity.intValue
+                  temperature:temperature.doubleValue
+                     humidity:humidity.doubleValue
+                     pressure:pressure.doubleValue
+                    windSpeed:windSpeed.doubleValue
+                  windBearing:windBearing
+                      uvIndex:uvIndex.intValue];
+    return self;
+}
 
 @end
