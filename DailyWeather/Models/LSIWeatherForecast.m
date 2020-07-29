5b6a4ebad5cab246b8b93d1b7e7f5d57e4e36647
Claudia Maciel <clc_80@yahoo.com>
2020-07-28 19:17:51 -0700

    Added LSIWeatherForecast model object

---
+++ DailyWeather/Models/LSIWeatherForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIWeatherForecast.m
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/28/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIWeatherForecast.h"
+
+@implementation LSIWeatherForecast
+
+@end
