19700b67ff468d1b0f3c6968b97fa07869ac8b46
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-29 16:07:52 -0400

    Started setting up WeatherResults

---
+++ DailyWeather/LSIWeatherResults.h
@@ -0,0 +1,27 @@
+//
+//  LSIWeatherResults.h
+//  DailyWeather
+//
+//  Created by Sean Acres on 7/29/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LSICurrentForecast;
+@class LSIHourlyForecast;
+@class LSIDailyForecast;
+
+@interface LSIWeatherResults : NSObject
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@property (nonatomic, readonly, copy) LSICurrentForecast *currentForecast;
+@property (nonatomic, readonly, copy) NSArray<LSIHourlyForecast *> *hourlyForecasts;
+@property (nonatomic, readonly, copy) NSArray<LSIDailyForecast *> *dailyForecasts;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/LSIWeatherResults.m
@@ -0,0 +1,20 @@
+//
+//  LSIWeatherResults.m
+//  DailyWeather
+//
+//  Created by Sean Acres on 7/29/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIWeatherResults.h"
+
+@implementation LSIWeatherResults
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSDictionary *currentWeatherDictionary = [dictionary objectForKey:@"currently"];
+    
+    
+}
+
+@end
