623061310615075cfbf49fa5548512abc7e4dd74
Claudia Maciel <clc_80@yahoo.com>
2020-07-28 19:50:40 -0700

    Wrote unit test to test CurrentWeather

--- DailyWeatherTests/DailyWeatherTests.m
+++ DailyWeatherTests/DailyWeatherTests.m
@@ -7,6 +7,10 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "../DailyWeather/LambdaSDK/LSIFileHelper.h"
+#import "../DailyWeather/LambdaSDK/LSILog.h"
+#import "../DailyWeather/Models/CurrentForecast.h"
+#import "../DailyWeather/Models/LSIWeatherForecast.h"
 
 @interface DailyWeatherTests : XCTestCase
 
@@ -14,11 +18,30 @@
 
 @implementation DailyWeatherTests
 
-- (void)testExample {
+- (void)testCurrectWeatherParsing {
+    NSData *weatherData = loadFile(@"CurrentWeather.json", [DailyWeatherTests class]);
 
-    // TODO: Use LSIFileHelper to load JSON from your test bundle
-    
-    // TODO: Create Unit Tests for each separate JSON file
+    NSError *jsonError = nil;
+    NSDictionary *currentDictionary = [NSJSONSerialization JSONObjectWithData:weatherData
+                                                                      options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:&jsonError]; 
+    if (!currentDictionary) {
+        NSLog(@"We've got an error: %@", jsonError);
+    }
+
+    if (![currentDictionary isKindOfClass:[NSDictionary class]]) {
+        NSLog(@"currentDictionary is not a dictionary!");
+        return;
+    }
+
+    CurrentForecast *current = [[CurrentForecast alloc] initWithDictionary:currentDictionary];
+
+    NSLog(@"current forecast: %@", current);
+
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:1581003354 / 1000.0];
+
+    XCTAssertEqual(48.35, current.temperature);
+    XCTAssertEqualObjects(@"Clear", current.summary);
+    XCTAssertEqualObjects(time, current.time);
 
 }
 
