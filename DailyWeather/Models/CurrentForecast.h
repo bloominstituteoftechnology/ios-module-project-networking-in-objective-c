9ef09568d9d6d8a5c112236fcfe5b9dee8c336d0
Claudia Maciel <clc_80@yahoo.com>
2020-07-28 19:18:38 -0700

    Created CurrentForecast object model

---
+++ DailyWeather/Models/CurrentForecast.h
@@ -0,0 +1,17 @@
+//
+//  CurrentForecast.h
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/28/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CurrentForecast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Models/CurrentForecast.m
@@ -0,0 +1,13 @@
+//
+//  CurrentForecast.m
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/28/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "CurrentForecast.h"
+
+@implementation CurrentForecast
+
+@end
