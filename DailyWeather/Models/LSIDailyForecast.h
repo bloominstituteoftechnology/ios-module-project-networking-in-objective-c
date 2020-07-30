3e8637d6ad515bed87d7173c98a0c4ab914fc7ea
Claudia Maciel <clc_80@yahoo.com>
2020-07-29 18:15:42 -0700

    Added new Daily Forecast class

---
+++ DailyWeather/Models/LSIDailyForecast.h
@@ -0,0 +1,17 @@
+//
+//  LSIDailyForecast.h
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/29/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIDailyForecast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Models/LSIDailyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIDailyForecast.m
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/29/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyForecast.h"
+
+@implementation LSIDailyForecast
+
+@end
