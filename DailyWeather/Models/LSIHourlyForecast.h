3faa3fa162e3011b875904912f1fb433ba0471de
Claudia Maciel <clc_80@yahoo.com>
2020-07-30 13:43:48 -0700

    Created DailyWeather class files

---
+++ DailyWeather/Models/LSIHourlyForecast.h
@@ -0,0 +1,17 @@
+//
+//  LSIHourlyForecast.h
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/30/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIHourlyForecast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Models/LSIHourlyForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSIHourlyForecast.m
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/30/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIHourlyForecast.h"
+
+@implementation LSIHourlyForecast
+
+@end
