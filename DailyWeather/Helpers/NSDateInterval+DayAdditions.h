6b0dbc69f4eea832a63796aebd3fbaeea3cb55a0
Claudia Maciel <clc_80@yahoo.com>
2020-07-30 20:12:39 -0700

    Added helper for NSDateInterval

---
+++ DailyWeather/Helpers/NSDateInterval+DayAdditions.h
@@ -0,0 +1,17 @@
+//
+//  NSDateInterval+DayAdditions.h
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/30/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDateInterval (DayAdditions)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Helpers/NSDateInterval+DayAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSDateInterval+DayAdditions.m
+//  DailyWeather
+//
+//  Created by Claudia Maciel on 7/30/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "NSDateInterval+DayAdditions.h"
+
+@implementation NSDateInterval (DayAdditions)
+
+@end
