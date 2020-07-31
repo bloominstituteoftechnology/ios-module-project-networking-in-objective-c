07e159d99fe47483ad46b7388e54e13264b77218
Claudia Maciel <clc_80@yahoo.com>
2020-07-30 20:14:13 -0700

    Implemented helper file

--- DailyWeather/Helpers/NSDateInterval+DayAdditions.h
+++ DailyWeather/Helpers/NSDateInterval+DayAdditions.h
@@ -12,6 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSDateInterval (DayAdditions)
 
++ (NSDateInterval *)lsi_dateIntervalByAddingDays:(NSInteger)days;
++ (NSDateInterval *)lsi_dateIntervalByAddingDays:(NSInteger)days toDate:(NSDate *)date;
+
 @end
 
 NS_ASSUME_NONNULL_END
