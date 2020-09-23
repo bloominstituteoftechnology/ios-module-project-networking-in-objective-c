3cd73fe688ab1215c6167f28e2c57b036dd60ee1
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-23 12:29:13 -0700

    Created CurrentForcast and added properties to decode json

---
+++ DailyWeather/Model/LSICurrentForecast.m
@@ -0,0 +1,13 @@
+//
+//  LSICurrentForecast.m
+//  DailyWeather
+//
+//  Created by Clayton Watkins on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSICurrentForecast.h"
+
+@implementation LSICurrentForecast
+
+@end
