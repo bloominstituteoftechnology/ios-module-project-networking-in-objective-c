3e5246673c9ca64b158b660a902ce05bffda9977
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-23 12:38:33 -0700

    Created LSIDailyWeather and added appropriate properties

---
+++ DailyWeather/Model/LSIDailyWeather.m
@@ -0,0 +1,15 @@
+//
+//  LSIDailyWeather.m
+//  DailyWeather
+//
+//  Created by Clayton Watkins on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIDailyWeather.h"
+
+@implementation LSIDailyWeather
+
+
+
+@end
