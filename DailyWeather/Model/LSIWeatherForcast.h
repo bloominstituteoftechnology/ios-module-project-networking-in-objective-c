37789f938bcf012cd7de8347734cfa0ca4f50106
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-23 12:25:22 -0700

    Added LSIWeatherForcast and fleshed out it's properties

---
+++ DailyWeather/Model/LSIWeatherForcast.h
@@ -0,0 +1,44 @@
+//
+//  LSIWeatherForcast.h
+//  DailyWeather
+//
+//  Created by Clayton Watkins on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIWeatherForcast : NSObject
+
+- (instancetype)initWithTime:(NSDate *)time
+                     summary:(NSString *)summary
+                        icon:(NSString *)icon
+           precipProbability:(int)precipProbability
+             precipIntensity:(int)precipIntensity
+                 temperature:(double)temperature
+                    humidity:(double)humity
+                    pressure:(double)pressure
+                   windSpeed:(double)windSpeed
+                 windBearing:(NSString *)windBearing
+                     uvIndex:(NSInteger *)uvIndex;
+
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@property (nonatomic, readonly) NSDate *time;
+@property (nonatomic, readonly, copy) NSString *summary;
+@property (nonatomic, readonly, copy) NSString *icon;
+@property (nonatomic, readonly) int precipProbability;
+@property (nonatomic, readonly) int precipIntensity;
+@property (nonatomic, readonly) double temperature;
+@property (nonatomic, readonly) double humidity;
+@property (nonatomic, readonly) double pressure;
+@property (nonatomic, readonly) double windSpeed;
+@property (nonatomic, readonly, copy) NSString *windBearing;
+@property (nonatomic, readonly) NSInteger *uvIndex;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Model/LSIWeatherForcast.m
@@ -0,0 +1,13 @@
+//
+//  LSIWeatherForcast.m
+//  DailyWeather
+//
+//  Created by Clayton Watkins on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIWeatherForcast.h"
+
+@implementation LSIWeatherForcast
+
+@end
