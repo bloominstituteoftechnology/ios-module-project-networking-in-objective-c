6105471cfca767d231cf3a3c02fb725988ece5dd
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-23 12:27:26 -0700

    Created wrong class, fixed issue.

--- DailyWeather/Model/LSIWeatherForcast.h
+++ DailyWeather/Model/LSIWeatherForcast.h
@@ -12,33 +12,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIWeatherForcast : NSObject
 
-- (instancetype)initWithTime:(NSDate *)time
-                     summary:(NSString *)summary
-                        icon:(NSString *)icon
-           precipProbability:(int)precipProbability
-             precipIntensity:(int)precipIntensity
-                 temperature:(double)temperature
-                    humidity:(double)humity
-                    pressure:(double)pressure
-                   windSpeed:(double)windSpeed
-                 windBearing:(NSString *)windBearing
-                     uvIndex:(NSInteger *)uvIndex;
 
-
-- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary;
-
-@property (nonatomic, readonly) NSDate *time;
-@property (nonatomic, readonly, copy) NSString *summary;
-@property (nonatomic, readonly, copy) NSString *icon;
-@property (nonatomic, readonly) int precipProbability;
-@property (nonatomic, readonly) int precipIntensity;
-@property (nonatomic, readonly) double temperature;
-@property (nonatomic, readonly) double humidity;
-@property (nonatomic, readonly) double pressure;
-@property (nonatomic, readonly) double windSpeed;
-@property (nonatomic, readonly, copy) NSString *windBearing;
-@property (nonatomic, readonly) NSInteger *uvIndex;
-
-@end
 
 NS_ASSUME_NONNULL_END
