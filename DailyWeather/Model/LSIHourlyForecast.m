df881dfbe0497c811d1a6d284c1a792953e9389f
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-23 12:42:25 -0700

    Created HourlyForecast file with properties

---
+++ DailyWeather/Model/LSIHourlyForecast.m
@@ -0,0 +1,15 @@
+//
+//  LSIHourlyForecast.m
+//  DailyWeather
+//
+//  Created by Clayton Watkins on 9/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIHourlyForecast.h"
+
+@implementation LSIHourlyForecast
+
+
+
+@end
