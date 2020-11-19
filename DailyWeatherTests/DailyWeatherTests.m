b54cbe30f1e993238ce31536bf16bb64714a069d
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-11-19 15:37:02 -0800

    Daily Weather Parsing and Unit Test

--- DailyWeatherTests/DailyWeatherTests.m
+++ DailyWeatherTests/DailyWeatherTests.m
@@ -10,6 +10,7 @@
 #import "../DailyWeather/LambdaSDK/LSILog.h"
 #import "../DailyWeather/LambdaSDK/LSIFileHelper.h"
 #import "../DailyWeather/CurrentForecast.h"
+#import "../DailyWeather/DailyForecast.h"
 
 @interface DailyWeatherTests : XCTestCase
 
@@ -55,6 +56,39 @@
     XCTAssertEqualObjects(@"clear-day", currentForecast.icon);
     XCTAssertEqualWithAccuracy(48.35, currentForecast.temperature, 0.0001);
     XCTAssertEqualWithAccuracy(10, currentForecast.visibility, 0.0001);
+}
+
+- (void)testDailyWeatherParsing
+{
+    NSData *weatherData = loadFile(@"DailyWeather.json", DailyWeatherTests.class);
+    
+    NSError *jsonError = nil;
+    NSDictionary *aDictionary = [NSJSONSerialization JSONObjectWithData:weatherData options:0 error:&jsonError];
+    
+    XCTAssertNotNil(aDictionary);
+    if (!aDictionary) {
+        NSLog(@"Error: %@", jsonError);
+        return;
+    }
+    
+    XCTAssertTrue([aDictionary isKindOfClass:NSDictionary.class]);
+    if (![aDictionary isKindOfClass:NSDictionary.class]) {
+        NSLog(@"aDictionary is not a dictionary!!");
+        return;
+    }
+    
+    NSLog(@"Daily weather (as dictionary): %@", aDictionary);
+    
+    DailyForecast *dailyForecast = [[DailyForecast alloc] initWithDictionary:aDictionary];
+    NSLog(@"Daily forecast (as DailyForecast): %@", dailyForecast);
+    
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:1580976000];
+    
+    XCTAssertEqualObjects(time, dailyForecast.time);
+    XCTAssertEqualObjects(@"Clear throughout the day.", dailyForecast.summary);
+    XCTAssertEqualObjects(@"clear-day", dailyForecast.icon);
+    XCTAssertEqualWithAccuracy(47.02, dailyForecast.temperatureLow, 0.0001);
+    XCTAssertEqualWithAccuracy(4, dailyForecast.uvIndex, 0.0001);
 }
 
 @end
