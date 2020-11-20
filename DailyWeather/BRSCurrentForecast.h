f0805a7df3f597659a11e7aa33f14f01d9577f57
brys <brys1saclaus2@gmail.com>
2020-11-19 16:26:32 -0800

    // created a currentForecastClass

---
+++ DailyWeather/BRSCurrentForecast.h
@@ -0,0 +1,19 @@
+//
+//  BRSCurrentForecast.h
+//  DailyWeather
+//
+//  Created by BrysonSaclausa on 11/19/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BRSCurrentForecast : NSObject
+
+- (instancetype)initWith
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/BRSCurrentForecast.m
@@ -0,0 +1,13 @@
+//
+//  BRSCurrentForecast.m
+//  DailyWeather
+//
+//  Created by BrysonSaclausa on 11/19/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "BRSCurrentForecast.h"
+
+@implementation BRSCurrentForecast
+
+@end
